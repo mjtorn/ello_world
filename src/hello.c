d754dec445cfcfeb37c5d136e7064aa4ea90680c
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-21 17:45:30 +0200

    Step II: A small, yet short-lived window
    I changed the name of win_del to _cb_win_del because that
    seems to be the modern way of naming these.

--- src/hello.c
+++ src/hello.c
@@ -8,9 +8,26 @@
 
 #include <Elementary.h>
 
+static void
+_cb_win_del(void *data, Evas_Object *obj, void *event_info)
+{
+   elm_exit();
+}
+
 EAPI int
 elm_main(int argc, char **argv)
 {
+   Evas_Object *win;
+
+   // (*parent, *name, type)
+   // NULL means not attaching to anything
+   // "hello" the name should be unique
+   win = elm_win_add(NULL, "hello", ELM_WIN_BASIC);
+   elm_win_title_set(win, "Hello");
+   evas_object_smart_callback_add(win, "delete,request", _cb_win_del, NULL);
+
+   evas_object_show(win);
+
    elm_run();
    elm_shutdown();
 
