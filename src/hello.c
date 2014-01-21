23b5033a32535febde1c42d83fd0f1e91ad462e3
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-21 18:06:49 +0200

    Step III: Add the bg and a label to the win
    Why elm_label_add(win) and not bg? Exercise for the reader?

--- src/hello.c
+++ src/hello.c
@@ -17,7 +17,7 @@ _cb_win_del(void *data, Evas_Object *obj, void *event_info)
 EAPI int
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win;
+   Evas_Object *win, *bg, *label;
 
    // (*parent, *name, type)
    // NULL means not attaching to anything
@@ -25,6 +25,19 @@ elm_main(int argc, char **argv)
    win = elm_win_add(NULL, "hello", ELM_WIN_BASIC);
    elm_win_title_set(win, "Hello");
    evas_object_smart_callback_add(win, "delete,request", _cb_win_del, NULL);
+
+   // Add a background widget that allows the window to resize
+   bg = elm_bg_add(win);
+   evas_object_size_hint_weight_set(bg, 1.0, 1.0);
+   elm_win_resize_object_add(win, bg);
+   evas_object_show(bg);
+
+   // And a label that actually makes it all stretch
+   label = elm_label_add(win);
+   elm_object_text_set(label, "Hello World!");
+   evas_object_size_hint_weight_set(label, 1.0, 1.0);
+   elm_win_resize_object_add(win, label);
+   evas_object_show(label);
 
    evas_object_show(win);
 
